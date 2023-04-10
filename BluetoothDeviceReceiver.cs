fa0aa58f2a657ed1660c154b8480dc6a55acd5df
AlexShuster <aps83@pitt.edu>
2023-04-10 10:15:09 -0400

    Reads incoming data

--- BluetoothDeviceReceiver.cs
+++ BluetoothDeviceReceiver.cs
@@ -7,45 +7,15 @@ using System.Linq;
 
 namespace DopplerRadarAndroidApp
 {
-    public class BluetoothDeviceReceiver : BroadcastReceiver
+    public class BluetoothHandler
     {
         public static BluetoothAdapter Adapter => BluetoothAdapter.DefaultAdapter;
 
         public List<BluetoothDevice> deviceList = new List<BluetoothDevice>();
         
-        public BluetoothDeviceReceiver()
+        public BluetoothHandler()
         {
             deviceList = Adapter.BondedDevices.ToList();
-        }
-
-        public override void OnReceive(Context context, Intent intent)
-        {
-            var action = intent.Action;
-
-            // Found a device
-            switch (action)
-            {
-                case BluetoothDevice.ActionFound:
-                    // Get the device
-                    var device = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
-
-                    // Only update the adapter with items which are not bonded
-                    if (device.BondState != Bond.Bonded)
-                    {
-                        MainActivity.GetInstance().UpdateAdapter(new DataItem(device.Name, device.Address));
-                        deviceList.Add(device);
-                    }
-
-                    break;
-                case BluetoothAdapter.ActionDiscoveryStarted:
-                    MainActivity.GetInstance().UpdateAdapterStatus("Discovery Started...");
-                    break;
-                case BluetoothAdapter.ActionDiscoveryFinished:
-                    MainActivity.GetInstance().UpdateAdapterStatus("Discovery Finished.");
-                    break;
-                default:
-                    break;
-            }
         }
     }
 }
--- MainActivity.cs
+++ MainActivity.cs
@@ -42,9 +42,9 @@ namespace DopplerRadarAndroidApp
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        private const int LocationPermissionsRequestCode = 1000;
+        private const int BluetoothPermissionsRequestCode = 1000;
 
-        private static readonly string[] LocationPermissions =
+        private static readonly string[] BluetoothPermissions =
         {
             Manifest.Permission.AccessCoarseLocation,
             Manifest.Permission.AccessFineLocation,
@@ -60,9 +60,8 @@ namespace DopplerRadarAndroidApp
         // Bluetooth Variables
         private static MainActivity _instance;
         private bool _isReceiveredRegistered;
-        private BluetoothDeviceReceiver _receiver;
+        private BluetoothHandler _handler;
         private BluetoothSocket _socket;
-        private BluetoothServerSocket _serverSocket;
 
         public static MainActivity GetInstance()
         {
@@ -71,6 +70,7 @@ namespace DopplerRadarAndroidApp
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            // SchiChart License Key
             SciChartSurface.SetRuntimeLicenseKey("SrDiaOt4OKLdVapiKRimqKbg/4T5ljr/pv4erw0WJa6YWIk0DIfv/kY/Yrhn87mcrHTWcSZz/8K/OMQwErwJiTwBtrKs2634dkIISi3Ti46yAg4KUPYrHDliaRvBWA0VzD8ScQSHrWa/GuDnkyzL1MQOuPb6dV6HJh3Q9zQtNtm7QIOyHQkUlevKXjEXiUo/G9yyNTE1c4FcdsaIP0DFMthVBzUnAuTIaogY6HvKLvPKDreLyw0wNE3CjDo/Hb8cF7DM/inkyrt/7NNjNe2qfLQsqUQlxLeNUWDWUZqSnNbil07g8qHprNjpCcDGlSQ8c0ySpfe+1LYSD2icWqC3158eUTKmAkAlXMp5EwCFGIwb0Q3ddu/HOaiHJAEe9Njxj/aNIvJKepk16XwZFci36pbFPUrR0kbNkBWLPC835G/X12ysfGWFjODS5rw31FUNeaO1frsKcLui2nVMJ+g4yp4qygGCQY09gKznDIA77aISdhFxddCZxm/SCweQ+8bmxmkvnsIxKvPobnJ7bNnPA81ZW8TTBc/XG/Fz2I5PNWTnEdELtt9n6IN0gt14J+LMtw3fdJfRp6r5o6etYBrE9ZEV6cT7");
 
             base.OnCreate(savedInstanceState);
@@ -79,21 +79,11 @@ namespace DopplerRadarAndroidApp
 
             SetContentView(Resource.Layout.activity_main);
 
-            var coarseLocationPermissionGranted =
-                ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation);
-            var fineLocationPermissionGranted =
-                ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation);
-
-            if (coarseLocationPermissionGranted != Permission.Denied ||
-                fineLocationPermissionGranted == Permission.Denied)
-                ActivityCompat.RequestPermissions(this, LocationPermissions, LocationPermissionsRequestCode);
+            // Bluetooth Permissions
+            ActivityCompat.RequestPermissions(this, BluetoothPermissions, BluetoothPermissionsRequestCode);
 
             // Register for broadcasts when a device is discovered
-            _receiver = new BluetoothDeviceReceiver();
-
-            RegisterBluetoothReceiver();
-
-            PopulateListView();
+            _handler = new BluetoothHandler();
 
             Button scanButton = FindViewById<Button>(Resource.Id.ScanButton);
             scanButton.Click += ScanButton_Click;
@@ -105,9 +95,6 @@ namespace DopplerRadarAndroidApp
 
             // Make sure we're not doing discovery anymore
             CancelScanning();
-
-            // Unregister broadcast listeners
-            UnregisterBluetoothReceiver();
         }
 
         protected override void OnPause()
@@ -116,9 +103,6 @@ namespace DopplerRadarAndroidApp
 
             // Make sure we're not doing discovery anymore
             CancelScanning();
-
-            // Unregister broadcast listeners
-            UnregisterBluetoothReceiver();
         }
 
         protected override void OnResume()
@@ -126,101 +110,22 @@ namespace DopplerRadarAndroidApp
             base.OnResume();
 
             StartScanning();
-
-            // Register broadcast listeners
-            RegisterBluetoothReceiver();
-        }
-
-        public void UpdateAdapter(DataItem dataItem)
-        {
-            var lst = FindViewById<Android.Widget.ListView>(Resource.Id.lstview);
-            var adapter = lst.Adapter as ListViewAdapter;
-
-            var items = adapter?.Items.Where(m => m.GetListItemType() == ListItemType.DataItem).ToList();
-
-            if (items != null && !items.Any(x =>
-                    ((DataItem)x).Text == dataItem.Text && ((DataItem)x).SubTitle == dataItem.SubTitle))
-            {
-                adapter.Items.Add(dataItem);
-            }
-
-            lst.Adapter = new ListViewAdapter(this, adapter?.Items);
         }
 
         private static void StartScanning()
         {
-            if (!BluetoothDeviceReceiver.Adapter.IsDiscovering) BluetoothDeviceReceiver.Adapter.StartDiscovery();
+            // if (!BluetoothHandler.Adapter.IsDiscovering) BluetoothHandler.Adapter.StartDiscovery();
         }
 
         private static void CancelScanning()
         {
-            if (BluetoothDeviceReceiver.Adapter.IsDiscovering) BluetoothDeviceReceiver.Adapter.CancelDiscovery();
+            if (BluetoothHandler.Adapter.IsDiscovering) BluetoothHandler.Adapter.CancelDiscovery();
         }
-
-        private void RegisterBluetoothReceiver()
-        {
-            if (_isReceiveredRegistered) return;
-
-            RegisterReceiver(_receiver, new IntentFilter(BluetoothDevice.ActionFound));
-            RegisterReceiver(_receiver, new IntentFilter(BluetoothAdapter.ActionDiscoveryStarted));
-            RegisterReceiver(_receiver, new IntentFilter(BluetoothAdapter.ActionDiscoveryFinished));
-            _isReceiveredRegistered = true;
-        }
-
-        private void UnregisterBluetoothReceiver()
-        {
-            if (!_isReceiveredRegistered) return;
-
-            UnregisterReceiver(_receiver);
-            _isReceiveredRegistered = false;
-        }
-
-        private void PopulateListView()
-        {
-            var item = new List<IListItem>
-            {
-                new HeaderListItem("PREVIOUSLY PAIRED")
-            };
-
-            item.AddRange(
-                BluetoothDeviceReceiver.Adapter.BondedDevices.Select(
-                    bluetoothDevice => new DataItem(
-                        bluetoothDevice.Name,
-                        bluetoothDevice.Address
-                    )
-                )
-            );
-
-            StartScanning();
-
-            item.Add(new StatusHeaderListItem("Scanning started..."));
-
-            var lst = FindViewById<Android.Widget.ListView>(Resource.Id.lstview);
-            lst.Adapter = new ListViewAdapter(this, item);
-        }
-
-        public void UpdateAdapterStatus(string discoveryStatus)
-        {
-            var lst = FindViewById<Android.Widget.ListView>(Resource.Id.lstview);
-            var adapter = lst.Adapter as ListViewAdapter;
-
-            var hasStatusItem = adapter?.Items?.Any(m => m.GetListItemType() == ListItemType.Status);
-
-            if (hasStatusItem.HasValue && hasStatusItem.Value)
-            {
-                var statusItem = adapter.Items.Single(m => m.GetListItemType() == ListItemType.Status);
-                statusItem.Text = discoveryStatus;
-            }
-
-            lst.Adapter = new ListViewAdapter(this, adapter?.Items);
-        }
-
-
 
         public void ScanButton_Click(object sender, System.EventArgs e)
         {
             // Get Bonded Device (RadarGunBluetooth)
-            var connectedDevice = (from bd in _receiver.deviceList where bd.Name == "RadarGunBluetooth" select bd).FirstOrDefault();// Return bonded device
+            var connectedDevice = (from bd in _handler.deviceList where bd.Name == "RadarGunBluetooth" select bd).FirstOrDefault();// Return bonded device
             //connectedDevice.SetPairingConfirmation(false);
 
             //connectedDevice.SetPairingConfirmation(true);
@@ -230,20 +135,26 @@ namespace DopplerRadarAndroidApp
             _socket = connectedDevice.CreateRfcommSocketToServiceRecord(Java.Util.UUID.FromString("00001101-0000-1000-8000-00805f9b34fb")); //the UUID of HC-05
             _socket.Connect();
 
-            //var connectedDevice = _receiver.deviceList.Find(x => x.Name == "AlexLab4");
-            ////_socket = connectedDevice.CreateL2capChannel(0x0080); c2478b88-3ede-4070-b011-3716be7c1aca
-            //_serverSocket = BluetoothDeviceReceiver.Adapter.ListenUsingRfcommWithServiceRecord("PitchData", UUID.FromString("0000FFE0-0000-1000-8000-00805F9B34FB"));
-            //_socket = _serverSocket.Accept();
-            //_serverSocket.Close();
-
-            //CancelScanning();
-            ////_socket.Connect();
-
-            //// Read data from the device
-            //// _socket.InputStream.ReadByte();
-
-            //// Write data to the device
+            // Write data to the device
             _socket.OutputStream.Write(System.Text.Encoding.ASCII.GetBytes("Hello World!"));
+            byte[] bytes = new byte[8];
+            byte[] bytes2 = new byte[8];
+            //float bytesString;
+            //float bytesString2;
+            string bytesString;
+            string bytesString2;
+
+            while (true)
+            {
+                _socket.InputStream.Read(bytes, 0, 4);
+                // bytesString = System.BitConverter.ToSingle(bytes, 0);
+                bytesString = System.Text.Encoding.ASCII.GetString(bytes);
+
+                _socket.InputStream.Read(bytes2, 0, 4);
+                // bytesString2 = System.BitConverter.ToSingle(bytes2, 0);
+                bytesString2 = System.Text.Encoding.ASCII.GetString(bytes2);
+            }
+            
         }
     }
 }
