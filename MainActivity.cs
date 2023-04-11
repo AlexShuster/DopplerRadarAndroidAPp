cdc00a63646b176178626015139a73322e4759e6
AlexShuster <aps83@pitt.edu>
2023-04-11 18:26:20 -0400

    Reads values more correctly than before

--- MainActivity.cs
+++ MainActivity.cs
@@ -36,12 +36,16 @@ using DopplerRadarAndroidApp.ListView;
 using static Android.Bluetooth.BluetoothClass;
 using Android.Media;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace DopplerRadarAndroidApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : Activity
     {
+
+        private PitchIdentifier _pitchID;
         private const int BluetoothPermissionsRequestCode = 1000;
 
         private static readonly string[] BluetoothPermissions =
@@ -87,6 +91,8 @@ namespace DopplerRadarAndroidApp
 
             Button scanButton = FindViewById<Button>(Resource.Id.ScanButton);
             scanButton.Click += ScanButton_Click;
+
+            _pitchID = new PitchIdentifier();
         }
 
         protected override void OnDestroy()
@@ -137,24 +143,160 @@ namespace DopplerRadarAndroidApp
 
             // Write data to the device
             _socket.OutputStream.Write(System.Text.Encoding.ASCII.GetBytes("Hello World!"));
-            byte[] bytes = new byte[8];
-            byte[] bytes2 = new byte[8];
-            //float bytesString;
-            //float bytesString2;
+            byte[] bytes = new byte[13];
+            byte[] bytes2 = new byte[13];
+
             string bytesString;
             string bytesString2;
 
+            string speed = "";
+            string speed2 = "";
+            string spinRate = "";
+            string spinRate2 = "";
+
+            float speedFinal = 9999;
+            float spinRateFinal = 9999;
+
+            bool isNewValue = true;
+
             while (true)
             {
-                _socket.InputStream.Read(bytes, 0, 4);
-                // bytesString = System.BitConverter.ToSingle(bytes, 0);
-                bytesString = System.Text.Encoding.ASCII.GetString(bytes);
+                // Read data from Bluetooth
+                _socket.InputStream.Read(bytes, 0, 13);
+                _socket.InputStream.Read(bytes2, 0, 13);
 
-                _socket.InputStream.Read(bytes2, 0, 4);
-                // bytesString2 = System.BitConverter.ToSingle(bytes2, 0);
+                // 1. Convert to string
+                bytesString = System.Text.Encoding.ASCII.GetString(bytes);
                 bytesString2 = System.Text.Encoding.ASCII.GetString(bytes2);
-            }
-            
+
+                // 2. Separate string into values using regex
+                Regex speedSplitter, speedSplitter2, spinRateSplitter, spinRateSplitter2;
+                Match speedMatch, speedMatch2, spinRateMatch, spinRateMatch2;
+                
+
+                if (bytesString.LastIndexOf(';') == 12) // aaaaa,bbbbbb;
+                {
+                    // Regex Splitters
+                    speedSplitter = new Regex("(.*),");
+                    spinRateSplitter = new Regex(",(.*);");
+                    speedMatch = speedSplitter.Match(bytesString);
+                    spinRateMatch = spinRateSplitter.Match(bytesString);
+
+                    // Get first read variables
+                    speed = speedMatch.Groups[1].ToString();
+                    spinRate = spinRateMatch.Groups[1].ToString();
+
+                    // Get second read variables
+                    speed2 = speedSplitter.Match(bytesString2).Groups[1].ToString();
+                    spinRate2 = spinRateSplitter.Match(bytesString2).Groups[1].ToString();
+                }
+                else if (bytesString.LastIndexOf(';') > 7 && bytesString.LastIndexOf(';') < 12) // a,bbbbbb;aaaa
+                {
+                    // Regex Splitters
+                    speedSplitter = new Regex(";(.*)");
+                    speedSplitter2 = new Regex("(.*),");
+                    spinRateSplitter = new Regex(",(.*);");
+                    speedMatch = speedSplitter.Match(bytesString);
+                    speedMatch2 = speedSplitter2.Match(bytesString);
+                    spinRateMatch = spinRateSplitter.Match(bytesString);
+
+                    // First Read variables
+                    speed = speedMatch.Groups[1].ToString() + speedMatch2.Groups[1].ToString();
+                    spinRate = spinRateMatch.Groups[1].ToString();
+
+                    // Second Read Variables
+                    speed2 = speedSplitter.Match(bytesString2).Groups[1].ToString() + speedSplitter2.Match(bytesString2).Groups[1].ToString();
+                    spinRate2 = spinRateSplitter.Match(bytesString2).Groups[1].ToString();
+                }
+                else if (bytesString.LastIndexOf(';') == 7) // ,bbbbbb;aaaaa
+                {
+                    // Regex Splitters
+                    speedSplitter = new Regex(";(.*)");
+                    spinRateSplitter = new Regex(",(.*);");
+                    speedMatch = speedSplitter.Match(bytesString);
+                    spinRateMatch = spinRateSplitter.Match(bytesString);
+
+                    // First Read Variables
+                    speed = speedMatch.Groups[1].ToString();
+                    spinRate = spinRateMatch.Groups[1].ToString();
+
+                    // Second Read Variables
+                    speed2 = speedSplitter.Match(bytesString2).Groups[1].ToString();
+                    spinRate2 = spinRateSplitter.Match(bytesString2).Groups[1].ToString();
+                }
+                else if (bytesString.LastIndexOf(';') == 6) // bbbbbb;aaaaa,
+                {
+                    // Regex Splitters
+                    speedSplitter = new Regex(";(.*)");
+                    spinRateSplitter = new Regex("(.*);");
+                    speedMatch = speedSplitter.Match(bytesString);
+                    spinRateMatch = spinRateSplitter.Match(bytesString);
+
+                    // First Read Variables
+                    speed = speedMatch.Groups[1].ToString();
+                    spinRate = spinRateMatch.Groups[1].ToString();
+
+                    // First Read Variables
+                    speed2 = speedSplitter.Match(bytesString2).Groups[1].ToString();
+                    spinRate2 = spinRateSplitter.Match(bytesString2).Groups[1].ToString();
+
+                }
+                else if (bytesString.LastIndexOf(';') > 2 && bytesString.LastIndexOf(';') < 6) // bbbbb;aaaaa,b
+                {
+                    // Regex Splitters
+                    speedSplitter = new Regex(";(.*),");
+                    spinRateSplitter = new Regex(",(.*)");
+                    spinRateSplitter2 = new Regex("(.*);");
+                    speedMatch = speedSplitter.Match(bytesString);
+                    spinRateMatch = spinRateSplitter.Match(bytesString);
+                    spinRateMatch2 = spinRateSplitter2.Match(bytesString);
+
+                    // First Read Variables
+                    speed = speedMatch.Groups[1].ToString();
+                    spinRate = spinRateMatch.Groups[1].ToString() + spinRateMatch.Groups[1].ToString();
+
+                    // Second Read Variables
+                    speed2 = speedSplitter.Match(bytesString2).Groups[1].ToString();
+                    spinRate2 = spinRateSplitter.Match(bytesString2).Groups[1].ToString() + spinRateSplitter2.Match(bytesString2).Groups[1].ToString();
+
+                }
+                else if (bytesString.LastIndexOf(';') == 0) // ;aaaaa,bbbbbb
+                {
+                    // Regex Splitters
+                    speedSplitter = new Regex(";(.*),");
+                    spinRateSplitter = new Regex(",(.*)");
+                    speedMatch = speedSplitter.Match(bytesString);
+                    spinRateMatch = spinRateSplitter.Match(bytesString);
+
+                    // First Read Variables
+                    speed = speedMatch.Groups[1].ToString();
+                    spinRate = spinRateMatch.Groups[1].ToString();
+
+                    // Second Read Variables
+                    speed2 = speedSplitter.Match(bytesString2).Groups[1].ToString();
+                    spinRate2 = spinRateSplitter.Match(bytesString2).Groups[1].ToString();
+                }
+
+                // 3. Check if first few values of vel/spin are equal
+                //string speedTest1 = speed.Substring(0, 3);
+                //string speedTest2 = speed2.Substring(0, 3);
+                //string spinTest1 = spinRate.Substring(0, 3);
+                //string spinTest2 = spinRate2.Substring(0, 3);
+                if (speed.Substring(0, 3) == speed2.Substring(0, 3) && spinRate.Substring(0, 3) == spinRate2.Substring(0, 3))
+                {
+                    if (speed.Substring(0, 3) != speedFinal.ToString().Substring(0, 3) && spinRate.Substring(0, 2) != spinRateFinal.ToString().Substring(0, 2))
+                    {
+                        speedFinal = float.Parse(speed);
+                        spinRateFinal = float.Parse(spinRate);
+
+                        isNewValue = false;
+                    }
+                    else
+                    {
+                        isNewValue = true;
+                    }
+                }
+            }    
         }
     }
 }
